bb21293ff220fc6bf3176252ba58a32f557343c4
System Administrator <root@SKP1002602MN001.local>
2015-11-03 13:34:52 +0900

    first commit

---
+++ helloworld.php
@@ -0,0 +1,11 @@
+<?php
+
+
+$a = array('word1'=>'Hello','word2'=>'world');
+
+//print_r($var);
+foreach ($a as $key => $word){
+	echo $key;
+} 
+
+?>
---
+++ index.php
@@ -0,0 +1,6 @@
+<?php
+include_once 'rnheader.php';
+
+echo "<h3>Home page</h3>
+		Welcom, please Sign up and/or Log in to join in.";
+?>
